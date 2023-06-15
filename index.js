0bc55b9fc11bd6d0e862022d9343ccadc3f326cb
Yessi2021 <yessiangulo2021@gmail.com>
2023-06-15 17:09:03 -0500

    prueba

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+
+const app = express()
+
+
+app.get('/',(req,res)=>{
+    res.json({
+        name:"Yessi esta es la pagina principal"
+    })
+})
+
+
+app.get('/api',(req,res)=>{
+    res.json({
+        name:"Yessi"
+    })
+})
+
+app.listen( process.env.PORT || 4000, ()=>{
+    console.log(`server running ${process.env.PORT}`);
+})
